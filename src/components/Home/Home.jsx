fc00626f1f7ce8cdd791ec3b8730a7ced3ae85e2
Md belayet <mdbelayethossain016@gmail.com>
2024-10-23 17:42:19 +0600

    task complete

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,16 +1,23 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigation } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import './Home.css'
 
 const Home = () => {
+    const navigation = useNavigation();
+    const location = useLocation();
+    console.log(location);
     return (
         <div>
             <div className="home">
                 <Header></Header>
                 <h4>Md Belatet <small>My website</small></h4>
             </div>
-            <Outlet></Outlet>
+            {
+                navigation.state ===  'loading'?
+                 <p>Loading....</p> :
+                  <Outlet></Outlet>
+            }
             <Footer></Footer>
         </div>
     );
