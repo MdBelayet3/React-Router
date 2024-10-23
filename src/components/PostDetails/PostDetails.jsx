ae564fb21e6d00fc2a6136333b68c7b33ac91de2
Md belayet <mdbelayethossain016@gmail.com>
2024-10-23 12:12:08 +0600

    post details finish

--- src/components/PostDetails/PostDetails.jsx
+++ src/components/PostDetails/PostDetails.jsx
@@ -1,7 +1,14 @@
+import { useLoaderData } from "react-router-dom";
+
 const PostDetails = () => {
+    const post = useLoaderData();
+    const {id, title, body, userId} = post;
     return (
         <div>
-            <h1>hi</h1>
+            <h3>User id : {userId} </h3>
+            <h3> Post id : {id}</h3>
+            <p>{title}</p>
+            <p><small>{body}</small></p>
         </div>
     );
 };
